c1cf6ae0a444b0409e57497b53aaf73af323e631
João Pedro <joao21dev@gmail.com>
2022-07-10 23:09:01 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [reverse, setReverse] = useState(false);
+  const [counter, setCounter] = useState(0);
+  const reverseClass = reverse ? 'reverse' : '';
+
+  const handleClick = () => {
+    setReverse(!reverse);
+  };
+
+  const handleIncrement = () => {
+    setCounter(counter + 1);
+  };
+  console.log('reverse value ', reverse);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className={`App-logo ${reverseClass}`} alt="logo" />
+        <h1>Counter: {counter} </h1>
+        <button style={{ marginBottom: '20px' }} onClick={handleClick}>
+          Reverse
+        </button>
+        <button onClick={handleIncrement}>Increment Counter</button>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
